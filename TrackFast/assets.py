19b5b66d1bdece6e5ecbdbac5b2d64c4cdc48e27
Marvin <mdtensuan_dev@protonmail.com>
2025-05-17 15:37:03 +0800

    Convert and combine Gemini outputs to DataFrame.

--- TrackFast/assets.py
+++ TrackFast/assets.py
@@ -1,1 +1,24 @@
+from pathlib import Path
 
+from dagster import asset, MaterializeResult, MetadataValue
+
+import polars as pl
+
+@asset
+def transactions() -> MaterializeResult:
+    """Return a DataFrame of transactions from `io/gemini`."""
+
+    gemini = Path("io/gemini")
+
+    txns = []
+
+    for file in gemini.iterdir():
+        if file.suffix == ".json":
+            df = pl.read_json(file)
+            txns.append(df)
+
+    transactions: pl.DataFrame = pl.concat(txns)
+
+    return MaterializeResult(
+        metadata={"file_path": str(gemini), "preview": MetadataValue.md(str(transactions.head(5)))}
+    )
